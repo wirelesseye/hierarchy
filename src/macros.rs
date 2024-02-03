602ed0a7c2e2244080dae9b3f00ed3678bc2b79a
wirelessEye <okuloj@outlook.com>
2024-02-03 19:00:02 +0800

    Allow private fields

--- src/macros.rs
+++ src/macros.rs
@@ -1,45 +1,33 @@
 #[macro_export]
 macro_rules! class_utils {
     (@impl_base $vis:vis $struct_name:ident {
-        $(let $field_name:ident : $field_type:ty ;)*
+        $(pub let $pub_field_name:ident : $pub_field_type:ty ;)*
 
-        $(pub final fn $pub_final_fn_name:ident $pub_final_params:tt -> $pub_final_return_type:ty {
-            $($pub_final_fn_stmt:stmt);* $(;)?
-        })*
+        $(pub final fn $pub_final_fn_name:ident $pub_final_params:tt -> $pub_final_return_type:ty $pub_final_return_block:block)*
 
-        $(pub fn $pub_fn_name:ident $pub_params:tt -> $pub_return_type:ty {
-            $($pub_fn_stmt:stmt);* $(;)?
-        })*
+        $(pub fn $pub_fn_name:ident $pub_params:tt -> $pub_return_type:ty $pub_fn_block:block)*
 
-        $(fn $pri_fn_name:ident $pri_params:tt -> $pri_return_type:ty {
-            $($pri_fn_stmt:stmt);* $(;)?
-        })*
+        $(fn $pri_fn_name:ident $pri_params:tt -> $pri_return_type:ty $pri_fn_block:block)*
     }) => {
         hierarchy::paste! {
             impl $struct_name {
-                $(fn [<get_ $field_name>](&self) -> &$field_type {
-                    &self.$field_name
+                $(fn [<get_ $pub_field_name>](&self) -> &$pub_field_type {
+                    &self.$pub_field_name
                 })*
 
-                $(fn $pub_final_fn_name $pub_final_params -> $pub_final_return_type {
-                    $($pub_final_fn_stmt);*
-                })*
+                $(fn $pub_final_fn_name $pub_final_params -> $pub_final_return_type $pub_final_return_block)*
 
-                $(fn $pri_fn_name $pri_params -> $pri_return_type {
-                    $($pri_fn_stmt);*
-                })*
+                $(fn $pri_fn_name $pri_params -> $pri_return_type $pri_fn_block)*
             }
 
             $vis trait [<$struct_name Trait>] {
                 fn [<get_ $struct_name:snake>](&self) -> &$struct_name;
 
-                $(fn [<get_ $field_name>](&self) -> &$field_type {
-                    self.[<get_ $struct_name:snake>]().[<get_ $field_name>]()
+                $(fn [<get_ $pub_field_name>](&self) -> &$pub_field_type {
+                    self.[<get_ $struct_name:snake>]().[<get_ $pub_field_name>]()
                 })*
 
-                $(fn $pub_fn_name $pub_params -> $pub_return_type {
-                    $($pub_fn_stmt);*
-                })*
+                $(fn $pub_fn_name $pub_params -> $pub_return_type $pub_fn_block)*
             }
 
             impl [<$struct_name Trait>] for $struct_name {
@@ -55,95 +43,71 @@ macro_rules! class_utils {
 macro_rules! class {
     (
         $vis:vis $struct_name:ident extends $super_name:ident $(< $inherit_chain:tt)* {
-            $(let $field_name:ident : $field_type:ty ;)*
+            $(pub let $pub_field_name:ident : $pub_field_type:ty ;)*
+            $(let $priv_field_name:ident : $priv_field_type:ty ;)*
 
             $(override $override_name:ident {
-                $(pub fn $override_fn_name:ident $override_params:tt -> $override_return_type:ty {
-                    $($override_fn_stmt:stmt);* $(;)?
-                })*
+                $(pub fn $override_fn_name:ident $override_params:tt -> $override_return_type:ty $override_fn_block:block)*
             })*
 
-            $(pub final fn $pub_final_fn_name:ident $pub_final_params:tt -> $pub_final_return_type:ty {
-                $($pub_final_fn_stmt:stmt);* $(;)?
-            })*
+            $(pub final fn $pub_final_fn_name:ident $pub_final_params:tt -> $pub_final_return_type:ty $pub_final_return_block:block)*
 
-            $(pub fn $pub_fn_name:ident $pub_params:tt -> $pub_return_type:ty {
-                $($pub_fn_stmt:stmt);* $(;)?
-            })*
+            $(pub fn $pub_fn_name:ident $pub_params:tt -> $pub_return_type:ty $pub_fn_block:block)*
 
-            $(fn $pri_fn_name:ident $pri_params:tt -> $pri_return_type:ty {
-                $($pri_fn_stmt:stmt);* $(;)?
-            })*
+            $($(final)? fn $pri_fn_name:ident $pri_params:tt -> $pri_return_type:ty $pri_fn_block:block)*
         }
     ) => {
         hierarchy::paste! {
             $vis struct $struct_name {
                 [<$super_name:snake>]: $super_name,
-                $($field_name: $field_type),*
+                $(pub $pub_field_name: $pub_field_type,)*
+                $($priv_field_name : $priv_field_type,)*
             }
         }
 
         hierarchy::class_utils!(@impl_base $vis $struct_name {
-            $(let $field_name : $field_type ;)*
+            $(pub let $pub_field_name : $pub_field_type ;)*
 
-            $(pub final fn $pub_final_fn_name $pub_final_params -> $pub_final_return_type {
-                $($pub_final_fn_stmt);*
-            })*
+            $(pub final fn $pub_final_fn_name $pub_final_params -> $pub_final_return_type $pub_final_return_block)*
 
-            $(pub fn $pub_fn_name $pub_params -> $pub_return_type {
-                $($pub_fn_stmt);*
-            })*
+            $(pub fn $pub_fn_name $pub_params -> $pub_return_type $pub_fn_block)*
 
-            $(fn $pri_fn_name $pri_params -> $pri_return_type {
-                $($pri_fn_stmt);*
-            })*
+            $(fn $pri_fn_name $pri_params -> $pri_return_type $pri_fn_block)*
         });
 
         hierarchy::inherit!(
             $struct_name,
             $super_name $(< $inherit_chain)*,
             $(override $override_name {
-                $(fn $override_fn_name $override_params -> $override_return_type {
-                    $($override_fn_stmt);*
-                })*
+                $(fn $override_fn_name $override_params -> $override_return_type $override_fn_block)*
             })*
         );
     };
     (
         $vis:vis $struct_name:ident {
-            $(let $field_name:ident : $field_type:ty ;)*
+            $(pub let $pub_field_name:ident : $pub_field_type:ty ;)*
+            $(let $priv_field_name:ident : $priv_field_type:ty ;)*
 
-            $(pub final fn $pub_final_fn_name:ident $pub_final_params:tt -> $pub_final_return_type:ty {
-                $($pub_final_fn_stmt:stmt);* $(;)?
-            })*
+            $(pub final fn $pub_final_fn_name:ident $pub_final_params:tt -> $pub_final_return_type:ty $pub_final_return_block:block)*
 
-            $(pub fn $pub_fn_name:ident $pub_params:tt -> $pub_return_type:ty {
-                $($pub_fn_stmt:stmt);* $(;)?
-            })*
+            $(pub fn $pub_fn_name:ident $pub_params:tt -> $pub_return_type:ty $pub_fn_block:block)*
 
-            $(fn $pri_fn_name:ident $pri_params:tt -> $pri_return_type:ty {
-                $($pri_fn_stmt:stmt);* $(;)?
-            })*
+            $($(final)? fn $pri_fn_name:ident $pri_params:tt -> $pri_return_type:ty $pri_fn_block:block)*
         }
     ) => {
         $vis struct $struct_name {
-            $($field_name: $field_type),*
+            $(pub $pub_field_name: $pub_field_type,)*
+            $($priv_field_name : $priv_field_type,)*
         }
 
         hierarchy::class_utils!(@impl_base $vis $struct_name {
-            $(let $field_name : $field_type ;)*
+            $(pub let $pub_field_name : $pub_field_type ;)*
 
-            $(pub final fn $pub_final_fn_name $pub_final_params -> $pub_final_return_type {
-                $($pub_final_fn_stmt);*
-            })*
+            $(pub final fn $pub_final_fn_name $pub_final_params -> $pub_final_return_type $pub_final_return_block)*
 
-            $(pub fn $pub_fn_name $pub_params -> $pub_return_type {
-                $($pub_fn_stmt);*
-            })*
+            $(pub fn $pub_fn_name $pub_params -> $pub_return_type $pub_fn_block)*
 
-            $(fn $pri_fn_name $pri_params -> $pri_return_type {
-                $($pri_fn_stmt);*
-            })*
+            $(fn $pri_fn_name $pri_params -> $pri_return_type $pri_fn_block)*
         });
     };
 }
